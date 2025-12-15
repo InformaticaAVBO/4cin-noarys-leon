8b35b6382b8fbd7950bbe906f8ae581a389f7fd0
noarys-leon <noarystailil@gmail.com>
2025-12-15 20:50:31 +0000

    1512 metodi aggiornati

---
+++ Lista2/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nArrayList: ");
+        // lista è un oggetto che contiene root, creare una lista significa creare un puntatore al prossimo nodo.
+        // cosa sono? una sequenza di nodi in cui ogni nodo è fatto di due elementi, un elemento e un puntatore. Quello dell'inizio si chiama root, quello della fine si chiama null (se non c'e' un nodo successivo).
+        // questi nodi sono degli oggetti che hanno un valore e un suggerimento al nodo successivo.
+        
+        Lista lista = new Lista();
+        
+        lista.addTail(new Nodo("IT: Ciao"));
+        lista.addTail(new Nodo("EN: Hello"));
+        lista.addTail(new Nodo("ES: Hola"));
+        
+        lista.addHead(new Nodo("Prova !!"));
+        
+        lista.add("Prova 2 !!");
+        lista.add("yolo");
+
+        lista.exists("EN: Hello");
+        lista.exists("Prova 2 !!");
+
+        lista.remove("Prova !!");
+        lista.remove("Prova 2 !!");
+
+        lista.exists("Prova 2 !!");
+        lista.exists("IT: Ciao");
+
+        System.out.println(lista);
+    }
+}
---
+++ Lista2/src/Lista.java
@@ -0,0 +1,88 @@
+public class Lista {
+    Nodo root;
+
+    public Lista() {
+        root = null;
+    }
+
+    public void add(String s) {
+        Nodo n = new Nodo(s); // cosi crea un nodo automaticamente
+        addTail(n); 
+    }
+    
+    public void addHead(Nodo n) { // aggiunge un nuovo nodo davanti, nella "head"
+        /*if  (root == null) {
+            root = n;
+            return;
+        } else {
+            n.setNext(root);
+            root = n;
+        }*/
+
+        Nodo temp = root;
+        root = n;
+        n.setNext(temp);
+    } 
+
+    public void addTail(Nodo n) { // aggiunge un nuovo nodo in fondo alla lista, alla "tail"
+        if (root == null) {
+            root = n;
+        } else {
+            Nodo temp = root;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(n);
+        }
+    }
+
+    public void remove(String s) { // per eliminare un nodo della lista
+        if (root == null) {
+            return;
+        }
+
+        if (root.getValore().equals(s)) {
+            root = root.getNext();
+            System.out.println("Il nodo con valore " + root.getValore() + " nella head è stato eliminato.");
+            return;
+        }
+
+        Nodo previo = root;
+        Nodo corrente = root.getNext();
+
+        while (corrente != null) {
+            if (corrente.getValore().equals(s)) {
+                previo.setNext(corrente.getNext());
+                System.out.println("Il nodo con valore " + corrente.getValore() + " è stato eliminato.");
+                return;
+            }
+
+            previo = corrente;
+            corrente = corrente.getNext();
+        }
+    }
+
+    public boolean exists(String s) { // per capire se una stringa/nodo è nella lista
+        Nodo temp = root;
+        while (temp != null) {
+            if (temp.getValore().equals(s)) {
+                System.out.println("Il nodo con valore " + temp.getValore() + " esiste.");
+                return true;
+            }
+            temp = temp.getNext();
+        }
+        System.out.println("Il nodo con valore " + s + " non esiste.");
+        return false;
+    }
+
+    public String toString() {
+        String s = "\nLa lista contiene: ";
+        Nodo temp = root;
+        while (temp != null) {
+            s += temp.getValore() + " - ";
+            temp = temp.getNext();
+        }
+        return s;
+    }
+
+}
